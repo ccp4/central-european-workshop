ffa0f497b550c6fe1644efba986c872ca4d52810
Paul Bond <git@paulsbond.co.uk>
2025-03-14 11:17:59 +0000

    Home content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,70 @@
 export function Home() {
   return (
-    <h1>
-      CCP4 Structural Biology Summer School 2025
-      <br />
-      <small>University of York, UK &ndash; 31 Jul to 8 Aug</small>
-    </h1>
+    <>
+      <h1>
+        <span>CCP4 Structural Biology Summer School 2025</span>
+        <br />
+        <span>University of York, UK &ndash; 31 July to 8 August</span>
+      </h1>
+      <img src="/panorama.jpg" alt="" />
+      <p>
+        It is a great pleasure to announce the return of the CCP4 Structural
+        Biology Summer School on macromolecular crystallography (MX), electron
+        cryo-microscopy, and associated techniques. The school aims to bring
+        together early-career scientists from the UK, Europe and beyond and
+        train them in the latest advances in structural biology, with a primary
+        focus on MX and cryo-EM.
+      </p>
+      <p>
+        This school was formerly the CCP4/BCA Summer School, which was run at
+        the University of St. Andrews for many years. It has trained hundreds of
+        crystallographers while also providing them with unforgettable
+        collective memories. The York team aims to capture and continue the
+        essence and tradition of the school, blending it with the best the City
+        of York has to offer.
+      </p>
+      <p>
+        All aspects of structure solution will be covered during the workshop,
+        from data collection through to phasing, refinement, validation and
+        deposition.
+      </p>
+      <ul>
+        <li>
+          Lectures and tutorials will be delivered by experts in the field,
+          sometimes even software authors themselves!
+        </li>
+        <li>
+          An exciting social programme will showcase some of the best the
+          historical City of York can offer - and in summer.
+        </li>
+        <li>
+          You will be able to work alongside world-leading scientists and
+          methods developers on your own projects.
+        </li>
+      </ul>
+      <p>
+        The school will be based at <b>The University of York's Campus West</b>,
+        with lectures in the main Biology building, workshops in a PC room in
+        James College and accommodation in Vanbrugh College.
+      </p>
+      <p>
+        <a title="Location information" href="location.php">
+          More details and directions
+        </a>
+      </p>
+      <p>
+        Spaces are limited. The applicant will have to demonstrate the need for
+        this sort of specialist training, and be prepared to submit the{" "}
+        <b>e-mail address of a supervisor</b>
+        who will write a <b>letter of recommendation</b> in support of their
+        application.
+      </p>
+      <p>We look forward to welcoming you in York this summer!</p>
+      <p>
+        The organisers,
+        <br />
+        Johan Turkenburg, Jon Agirre, Paul Bond, Chris Hill and Lucy Schofield.
+      </p>
+    </>
   );
 }
