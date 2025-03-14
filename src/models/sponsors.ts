8de6e8963a562626e92370bbaf0a6efdba56740b
Paul Bond <git@paulsbond.co.uk>
2025-03-14 12:15:56 +0000

    Change class to interface

--- src/models/sponsors.ts
+++ src/models/sponsors.ts
@@ -1,30 +1,36 @@
-export class Sponsor {
-  constructor(
-    public name: string,
-    public url: string,
-    public logo: string,
-  ) {}
+export interface Sponsor {
+  name: string;
+  url: string;
+  logo: string;
 }
 
-export const bbsrc = new Sponsor(
-  "BBSRC",
-  "https://www.ukri.org/councils/bbsrc",
-  "bbsrc.png",
-);
-export const ccp4 = new Sponsor("CCP4", "https://www.ccp4.ac.uk", "ccp4.png");
-export const rigaku = new Sponsor("Rigaku", "https://rigaku.com", "rigaku.png");
-export const sptlabtech = new Sponsor(
-  "SPT Labtech",
-  "https://sptlabtech.com",
-  "sptlabtech.svg",
-);
-export const thermofisher = new Sponsor(
-  "Thermo Fisher Scientific",
-  "https://thermofisher.com",
-  "thermofisher.svg",
-);
-export const york = new Sponsor(
-  "University of York",
-  "https://york.ac.uk",
-  "uoy.svg",
-);
+export const bbsrc: Sponsor = {
+  name: "BBSRC",
+  url: "https://www.ukri.org/councils/bbsrc",
+  logo: "bbsrc.png",
+};
+export const ccp4: Sponsor = {
+  name: "CCP4",
+  url: "https://www.ccp4.ac.uk",
+  logo: "ccp4.png",
+};
+export const rigaku: Sponsor = {
+  name: "Rigaku",
+  url: "https://rigaku.com",
+  logo: "rigaku.png",
+};
+export const sptlabtech: Sponsor = {
+  name: "SPT Labtech",
+  url: "https://sptlabtech.com",
+  logo: "sptlabtech.svg",
+};
+export const thermofisher: Sponsor = {
+  name: "Thermo Fisher Scientific",
+  url: "https://thermofisher.com",
+  logo: "thermofisher.svg",
+};
+export const york: Sponsor = {
+  name: "University of York",
+  url: "https://york.ac.uk",
+  logo: "uoy.svg",
+};
