e049ac5780d3214d5ae14124687e959c0711e5dc
Paul Bond <git@paulsbond.co.uk>
2025-03-17 10:22:49 +0000

    Using command instead of mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command }) => {
   return {
-    base: mode === "development" ? "/" : "/summer-school/",
+    base: command === "build" ? "/summer-school/" : "/",
     plugins: [react(), tailwindcss()],
   };
 });
