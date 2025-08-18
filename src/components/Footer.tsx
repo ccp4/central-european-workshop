295c60b1aee38288a07e83bce78833b20e1e77a9
Martin Malý <martin.maly.mm@email.cz>
2025-08-18 13:56:43 +0100

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,10 @@ import { Logo } from "./Logo";
 export function Footer() {
   return (
     <footer className="my-4 flex flex-col items-center gap-2">
+      <p className="text-gray-500">Additional support kindly received from:</p>
       <div className="flex flex-wrap justify-center gap-4">
         <Logo sponsor={ccpem} />
         <Logo sponsor={cssb} />
-        <Logo sponsor={iucr} />
-        <Logo sponsor={eca} />
         <Logo sponsor={dectris} />
       </div>
     </footer>
